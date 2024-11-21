9168b14cea500c4a3614bcabc38482a82b71588b
Skipeptp <glebkarpuhin8@gmail.com>
2024-11-21 17:49:29 +0300

    rffffff7

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -5,49 +5,55 @@
 
 using namespace std;
 
+bool is_matching(const string &opening, const string &closing) {
+    return opening == closing.substr(1, closing.size() - 2);
+}
+
 int main(int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+        return 1;
+    }
+
     ifstream input(argv[1]);
+    if (!input.is_open()) {
+        cerr << "Error opening file: " << argv[1] << endl;
+        return 1;
+    }
+
     Stack *stack = stack_create();
     string line;
-    bool isValid = true;
 
     while (getline(input, line)) {
-        if (line.empty()) {
-            continue;
-        }
+        if (!line.empty() && line.front() == '<' && line.back() == '>') {
+            if (line[1] == '/') { 
+                if (stack_empty(stack)) {
+                    cout << "NO" << endl;
+                    stack_delete(stack);
+                    return 0;
+                }
 
-        if (line[0] == '<' && line[1] != '/') {
-            if (line.size() < 2 || line[0] != '<' || line[line.size() - 1] != '>') {
-                isValid = false;
-                break;
+               
+                string *top_tag = static_cast<string*>(stack_get(stack));
+                if (!is_matching(*top_tag, line)) {
+                    cout << "NO" << endl;
+                    stack_delete(stack);
+                    return 0;
+                }
+                stack_pop(stack);
+            } else { 
+              
+                string *tag = new string(line);
+                stack_push(stack, static_cast<Data>(tag)); 
             }
-            string tagName = line.substr(1, line.size() - 2);
-            stack_push(stack, tagName);
-        } else if (line[0] == '<' && line[1] == '/') {
-            if (line.size() < 3 || line[2] != ' ') {
-                isValid = false;
-                break;
-            }
-            string closingTagName = line.substr(2, line.size() - 3);
-            if (stack_is_empty(stack) || stack_top(stack) != closingTagName) {
-                isValid = false;
-                break;
-            }
-            stack_pop(stack);
-        } else {
-            isValid = false;
-            break;
-        }
-
-        if (!isValid) {
-            break;
         }
     }
 
-    if (!stack_is_empty(stack)) {
-        isValid = false;
-
-    cout << (isValid ? "YES" : "NO");
+    if (!stack_empty(stack)) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
 
     stack_delete(stack);
     return 0;
