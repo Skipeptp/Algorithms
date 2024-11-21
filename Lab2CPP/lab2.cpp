c1408ba4e4816788e422c99c11a73b8c1597eefe
Skipeptp <glebkarpuhin8@gmail.com>
2024-11-21 17:27:47 +0300

    rffffff7

--- Lab2CPP/lab2.cpp
+++ Lab2CPP/lab2.cpp
@@ -5,59 +5,50 @@
 
 using namespace std;
 
-// Функция для проверки соответствия открывающего и закрывающего тега
-bool is_matching(const string &opening, const string &closing) {
-    return opening == closing.substr(1, closing.size() - 2);
-}
-
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
-        return 1;
-    }
-
     ifstream input(argv[1]);
-    if (!input.is_open()) {
-        cerr << "Error opening file: " << argv[1] << endl;
-        return 1;
-    }
-
     Stack *stack = stack_create();
     string line;
-    bool flag = false;
+    bool isValid = true;
 
     while (getline(input, line)) {
-        if (!line.empty() && line.front() == '<' && line.back() == '>') {
-            if (line[1] == '/') {
-                if (stack_empty(stack)) {
-                    flag = true;
-                    break;
-                }
+        if (line.empty()) {
+            continue;
+        }
 
-                // Изменение: убрал static_cast, так как stack_get возвращает Data, который должен быть std::string*
-                string *top_tag = static_cast<string*>(stack_get(stack));
-                if (!is_matching(*top_tag, line)) {
-                    flag = true;
-                    delete top_tag; // Освобождаем память
-                    break;
-                }
-                delete top_tag; // Освобождаем память
-                stack_pop(stack);
-            } else {
-                // Изменение: убрал static_cast, так как tag уже является std::string*
-                string *tag = new string(line);
-                stack_push(stack, tag); // без static_cast
+        if (line[0] == '<' && line[1] != '/') {
+            if (line.size() < 2 || line[0] != '<' || line[line.size() - 1] != '>') {
+                isValid = false;
+                break;
             }
+            string tagName = line.substr(1, line.size() - 2);
+            stack_push(stack, tagName);
+        } else if (line[0] == '<' && line[1] == '/') {
+            if (line.size() < 3 || line[2] != ' ') {
+                isValid = false;
+                break;
+            }
+            string closingTagName = line.substr(2, line.size() - 3);
+            if (stack_is_empty(stack) || stack_top(stack) != closingTagName) {
+                isValid = false;
+                break;
+            }
+            stack_pop(stack);
+        } else {
+            isValid = false;
+            break;
+        }
+
+        if (!isValid) {
+            break;
         }
     }
 
-    // Проверка на наличие ошибок
-    if (flag || !stack_empty(stack)) {
-        cout << "NO" << endl;
-    } else {
-        cout << "YES" << endl;
-    }
+    if (!stack_is_empty(stack)) {
+        isValid = false;
 
-    stack_delete(stack); // Освобождаем память
+    cout << (isValid ? "YES" : "NO");
+
+    stack_delete(stack);
     return 0;
 }
